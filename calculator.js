45da7bd746d64180e72cb566ce6b43b9ad4653b5
Daniel Gonzalez <dgonzaleztx1990@gmail.com>
2023-07-26 10:36:57 -0500

    created functions for all operations

---
+++ calculator.js
@@ -0,0 +1,18 @@
+//calculation functions//
+
+const add = function (a, b) {
+    return a + b
+};
+
+const minus = function (a, b){
+    return a - b
+};
+
+const divide = function (a, b) {
+    return a / b
+};
+
+const multipy = function (a, b) {
+    return a * b
+};
+
